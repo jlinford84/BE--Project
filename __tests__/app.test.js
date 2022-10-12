93ed7894039ec90065dcde01271cb669c6c84124
Jon <jlinford84@gmail.com>
2022-10-12 10:45:51 +0100

    6, 7 and 8 after feedback

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -30,31 +30,6 @@ describe("3 GET categories /api/categories", () => {
 });
 
 describe("4 GET reviews by id /api/reviews/:review_id", () => {
-  it("status 200, should confirm a return of reviews containing all categories", () => {
-    return request(app)
-      .get("/api/reviews")
-      .expect(200)
-      .then(({ body }) => {
-        expect(body.length).toEqual(13);
-        expect(
-          body.forEach((category) => {
-            expect(category).toEqual(
-              expect.objectContaining({
-                category: expect.any(String),
-                created_at: expect.any(String),
-                designer: expect.any(String),
-                owner: expect.any(String),
-                review_body: expect.any(String),
-                review_id: expect.any(Number),
-                review_img_url: expect.any(String),
-                title: expect.any(String),
-                votes: expect.any(Number),
-              })
-            );
-          })
-        );
-      });
-  });
   it("status 200, should return a review object, which should have the required properties", () => {
     const review_id = 1;
     return request(app)
@@ -62,21 +37,19 @@ describe("4 GET reviews by id /api/reviews/:review_id", () => {
       .expect(200)
       .then(({ body }) => {
         expect([body].length).toEqual(1);
-        expect(body).toEqual({
-          review: {
-            category: "euro game",
-            comment_count: 0,
-            created_at: "2021-01-18T10:00:20.514Z",
-            designer: "Uwe Rosenberg",
-            owner: "mallionaire",
-            review_body: "Farmyard fun!",
-            review_id: 1,
-            review_img_url:
-              "https://www.golenbock.com/wp-content/uploads/2015/01/placeholder-user.png",
-            title: "Agricola",
-            votes: 1,
-          },
-        });
+        expect(body.review).toEqual(
+          expect.objectContaining({
+            category: expect.any(String),
+            comment_count: expect.any(Number),
+            created_at: expect.any(String),
+            designer: expect.any(String),
+            owner: expect.any(String),
+            review_id: expect.any(Number),
+            review_img_url: expect.any(String),
+            title: expect.any(String),
+            votes: expect.any(Number),
+          })
+        );
       });
   });
   it("status 404, should return an error", () => {
@@ -119,45 +92,45 @@ describe("5 GET users /api/users", () => {
       });
   });
   it("status 404, should ", () => {
-    return request(app)
-      .get("/api/apple")
-      .expect(404)
+    return request(app).get("/api/apple").expect(404);
   });
 });
 
-describe('6 PATCH /api/reviews/:review_id', () => {
-  it('status 200, should add a number of votes to the selected review', () => {
+describe("6 PATCH /api/reviews/:review_id", () => {
+  it("status 200, should add a number of votes to the selected review", () => {
     return request(app)
       .patch("/api/reviews/1")
       .expect(200)
-      .send({ inc_votes: 3})
+      .send({ inc_votes: 3 })
       .then(({ body }) => {
         expect(body.review[0]).toEqual({
           review_id: 1,
-          title: 'Agricola',
-          category: 'euro game',
-          designer: 'Uwe Rosenberg',
-          owner: 'mallionaire',
-          review_body: 'Farmyard fun!',
-          review_img_url: 'https://www.golenbock.com/wp-content/uploads/2015/01/placeholder-user.png',
-          created_at: '2021-01-18T10:00:20.514Z',
-          votes: 4
+          title: "Agricola",
+          category: "euro game",
+          designer: "Uwe Rosenberg",
+          owner: "mallionaire",
+          review_body: "Farmyard fun!",
+          review_img_url:
+            "https://www.golenbock.com/wp-content/uploads/2015/01/placeholder-user.png",
+          created_at: "2021-01-18T10:00:20.514Z",
+          votes: 4,
         });
       });
   });
   it("status 404, should return an error", () => {
     const review_id = 999;
     return request(app)
-      .get(`/api/reviews/${review_id}`)
+      .patch(`/api/reviews/${review_id}`)
       .expect(404)
       .then(({ body }) => {
         expect(body).toEqual({ msg: "No review found for review_id:999" });
       });
   });
+  
   it("status 400, should return an error", () => {
     const review_id = "pear";
     return request(app)
-      .get(`/api/reviews/${review_id}`)
+      .patch(`/api/reviews/${review_id}`)
       .expect(400)
       .then(({ body }) => {
         expect(body).toEqual({ msg: "invalid type" });
@@ -165,13 +138,52 @@ describe('6 PATCH /api/reviews/:review_id', () => {
   });
 });
 
-describe('7. GET /api/reviews/review_id/comments', () => {
-  it('status 200, should return the comment count for a selected review', () => {
+describe("7. GET /api/reviews/review_id/comments", () => {
+  it("status 200, should return the comment count for a selected review", () => {
     return request(app)
-    .get('/api/reviews/3')
-    .expect(200)
-    .then(({ body }) => {
-      expect(body.review.comment_count).toEqual(3)
-    })
+      .get("/api/reviews/3")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.review.comment_count).toEqual(3);
+      });
+  });
+  
+  it("status 404, should return an error", () => {
+    return request(app)
+      .patch(`/api/reviews/1/condiments`)
+      .expect(404)
+  });
+});
+
+describe("8. GET /api/reviews", () => {
+  it("status 200, should confirm a return of reviews containing all categories", () => {
+    return request(app)
+      .get("/api/reviews")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.length).toEqual(13);
+        expect(
+          body.forEach((review) => {
+            expect(review).toEqual(
+              expect.objectContaining({
+                category: expect.any(String),
+                comment_count: expect.any(Number),
+                created_at: expect.any(String),
+                designer: expect.any(String),
+                owner: expect.any(String),
+                review_id: expect.any(Number),
+                review_img_url: expect.any(String),
+                title: expect.any(String),
+                votes: expect.any(Number),
+              })
+            );
+          })
+        );
+      });
+  });
+  it("status 404, should return an error", () => {
+    return request(app)
+      .patch(`/api/renews`)
+      .expect(404)
   });
 });
