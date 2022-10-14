a51c3112f69360a1a3f60b68cb1f8fa292b6a0d4
Jon <jlinford84@gmail.com>
2022-10-14 14:13:55 +0100

    updated for heroku

--- app.js
+++ app.js
@@ -20,9 +20,11 @@ const app = express();
 app.use(express.json());
 
 app.get("/api/categories", getCategories);
+
+app.get("/api/users/", getUsers);
+
 app.get("/api/reviews/", getReviews);
 app.get("/api/reviews/:review_id", getReviewById);
-app.get("/api/users/", getUsers);
 app.get("/api/reviews/:review_id/comments", getReviewComments);
 
 app.post("/api/reviews/:review_id/comments", postReviewComment);
